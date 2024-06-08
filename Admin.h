51bdd02b6e895a91c4ac94ecb0164f6c8c1022a5
ELDERPEREZ <125334333+Elder19@users.noreply.github.com>
2024-06-07 18:11:41 -0600

    Add files via upload

--- Admin.h
+++ Admin.h
@@ -1,15 +1,20 @@
 ﻿#pragma once
 
+#include <list>
 #include <string> 
 #include <iostream>
 #include <fstream> 
 #include <sstream> 
+#include <algorithm> // Para std::transform
+#include <cctype>    // Para std::tolower
 #include "readANSI.h"
 #include "objPalabra.h"
 #include "ArrayListt.h"
 #include "BSTDictionary.h"
-#include "MinHeap.h"
+#include "MaxHeap.h"
 #include "HeapPriorityQueue.h"
+#include "Trie.h"
+#include "BSTree.h"
 using namespace std;
 
 /*
@@ -22,13 +27,15 @@ class Admin {
 private:
     // Constructor privado para evitar la creaci�n de instancias fuera de la clase
     Admin() {}
-    ArrayListt<string>  ignoreW;  // Lista de apariciones en líneas
-    ArrayListt<char>  ignoreL;  //abecedario
+    BSTree<string>* bst; // Lista de apariciones en líneas
+    ArrayListt<char>* ignoreL  ;  //abecedario
     HeapPriorityQueue<string>* queue;// lista de prioridad
     DLinkedList<string>* Lineas = new DLinkedList<string>();
     // Evitar que se copie la instancia
     Admin(const Admin&) = delete;
     Admin& operator=(const Admin&) = delete;
+    Trie trie;
+    
   
 public:
     // Metodo para obtener la instancia
@@ -37,6 +44,29 @@ public:
         return instance;
 
     }
+    void verTop() {
+        string input;
+        int number;
+
+        // Ignorar cualquier entrada previa
+        cin.ignore();
+
+        // Pedir al usuario que ingrese la cantidad de palabras
+        cout << "Ingrese la cantidad de palabras que desea ver: ";
+        getline(cin, input);
+
+     
+        // Intentar convertir la cadena a un entero
+        number = stoi(input);
+
+            // Verificar que el número sea positivo
+        if (number <= 0) {
+            cout << ("El numero no es un entero positivo.\n");
+        
+        trie.topNWords(number, bst);
+        }
+    
+    }
     /**
      * @brief Lee el contenido de un archivo.
      *
@@ -44,31 +74,29 @@ public:
      * @param charge Si es true, carga las líneas del archivo en la lista de líneas.
      * @return El contenido del archivo como una cadena de texto.
      */
-    string leerArchivo(string ruta, bool charge) {
-        
-        ifstream archivo(ruta);
 
-        string contenido; //  para almacenar el contenido del archivo
+    std::string leerArchivo(const std::string& ruta, bool charge) {
+        std::ifstream archivo(ruta);
+        std::string contenido; // Para almacenar el contenido del archivo
+        std::string linea; // Para leer cada línea del archivo
 
-        string linea; //  para leer cada línea del archivo
+        std::cout << "Ruta: " << ruta << "\n";
 
-        cout << "Ruta: " << ruta << "\n";
         if (archivo.is_open()) {
-
-            while (getline(archivo, linea)) {
+            cout << ("entre");
+            while (std::getline(archivo, linea)) {
                 contenido += linea + "\n"; // Concatena la línea y un salto de línea
-                if (charge)
-                    Lineas->insert(linea + "\n");
-
+                if (charge) {
+                    Lineas->append(linea + "\n");
+                }
             }
             archivo.close(); // Cierra el archivo después de la lectura
-            return contenido;
         }
         else {
-
-            cout << "Error al abrir el archivo \n" << endl;
-            return "";
+            std::cout << "Error al abrir el archivo \n" << std::endl;
         }
+
+        return contenido;
     }
 
     /**
@@ -99,13 +127,19 @@ public:
 
             // Encontrar el final de la palabra
             size_t finPalabra = fin;
-            while (finPalabra < contenidoArchivo.length() && contenidoArchivo[finPalabra] != ' ' && contenidoArchivo[finPalabra] != '\n') {
+            while (finPalabra < contenidoArchivo.length() && ignoreL->contains(contenidoArchivo[finPalabra]) && contenidoArchivo[finPalabra] != '\n') {
+
                 finPalabra++;
                
             }
             
             if (fin < finPalabra) {
                 string palabra = contenidoArchivo.substr(fin, finPalabra - fin);
+                std::transform(palabra.begin(), palabra.end(), palabra.begin(), ::tolower);
+
+                trie.insert(palabra, lineNumber);
+                trie.insert(palabra, lineNumber);
+                trie.print();
                 line.clear();
                 cout << "Palabra: " << palabra << ", Línea: " << lineNumber << std::endl;
                 fin = finPalabra;
@@ -114,22 +148,27 @@ public:
     }
     /**
     * @brief Lee caracteres individuales de un archivo especificado y los almacena en la lista de caracteres a ignorar.
-    *
+    *for (char c = 'a'; c <= 'z'; ++c) {
+            ignoreL->insert(c);
+            ignoreL->insert(toupper(c)); // Insertar también la versión mayúscula
+        }
+        ignoreL->insert('ñ');
+        ignoreL->insert('Ñ'); // Insertar también la versión mayúscula de 'ñ'
+        ignoreL->insert('ü');
+        ignoreL->insert('Ü'); // Insertar también la versión mayúscula de 'ü'
+        return true;
     * Solicita al usuario una ruta de archivo y lo lee, almacenando cada carácter en la lista de caracteres a ignorar.
     */
     void abc() {
-        string ruta;
-        cin.ignore();
-        cout << "ingrese una direccion de archivo: ";
-        getline(cin, ruta);
-        string contenidoArchivo = leerArchivo(ruta, false);
+        string contenidoArchivo = leerArchivo("abecedario.txt", false);
         cout << contenidoArchivo;
         for (char c : contenidoArchivo) {
             if (c != ' ' && c != '\n') {
                 cout << c;
-                ignoreL.append(c);
+                ignoreL->append(c);
             }
         }
+        
     }
     /**
     * @brief Lee palabras de un archivo y las almacena en la lista de palabras a ignorar.
@@ -164,14 +203,14 @@ public:
 
             // Encontrar el final de la palabra
             inicio = fin;
-            while (fin < contenidoArchivo.length() && ignoreL.contains(contenidoArchivo[fin]) && contenidoArchivo[fin] != '\n') {
+            while (fin < contenidoArchivo.length() && ignoreL->contains(contenidoArchivo[fin]) && contenidoArchivo[fin] != '\n') {
 
                 fin++;
             }
 
             if (inicio < fin) {
                 string palabra = contenidoArchivo.substr(inicio, fin - inicio);
-                ignoreW.append(palabra);
+                bst->insert(palabra);
             }
 
             cout << "Se ingreso con exito el archivo de texto con palabras a ingresar";
